533110a705e40c1612d90e1d5d91ca122903fe60
PrajwalMReddy <48818107+PrajwalMReddy@users.noreply.github.com>
2022-10-04 10:46:53 +0530

    Read The Input .mdl File

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello World!");
+    let args: Vec<String> = env::args().collect();
+    let contents = read_file(&args);
+
+    println!("{}", contents);
+}
+
+fn read_file(args: &Vec<String>) -> String {
+    if args.len() == 1 {
+        panic!("A File Path Must Be Given");
+    }
+
+    let file_path = &args[1];
+    let contents = fs::read_to_string(file_path).expect(&("Unable To Read File ".to_owned() + file_path));
+
+    contents
 }
